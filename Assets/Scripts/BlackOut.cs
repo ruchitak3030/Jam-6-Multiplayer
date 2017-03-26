6bff3af364392484633429de2b9b5b1403c5f604
Ruchita Kondvilkar <ruchi3030@outlook.com>
2017-03-26 15:55:48 -0400

    BlackOut

---
+++ Assets/Scripts/BlackOut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackOut : MonoBehaviour {
+
+    // Use this for initialization
+    private float waitSec = 15.0f;
+
+	
+    public void Start()
+    {
+        gameObject.SetActive(false);
+       InvokeRepeating("LateCall", waitSec, waitSec);
+    }
+	
+	private void LateCall()
+    {
+        
+        if(gameObject.activeInHierarchy)
+        {
+            gameObject.SetActive(false);
+        }
+        else if(!gameObject.activeInHierarchy)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
